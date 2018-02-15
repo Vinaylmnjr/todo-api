ede5a04679d7bf8fe03715cd537cc88402063690
Vinaysastha <vvvsgvinaysastha@gmail.com>
2018-02-15 16:28:55 +0530

    Server js has been modified

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet her at the bus station',
+	compleated: false
+	},
+	{
+	id: 2,
+	description: 'return home',
+	compleated: false
+	}];
 
 
 app.get('/',function (req,res) {
 	res.send('Todo API Root');
 });
-  
+
 app.listen(PORT,function(){
 	console.log('Express listining on PORT: '+ PORT + '!');
 });  
 
+
+app.get('/todos/:id',function (req,res){
+	res.send('Asking for todo with id of '+ req.params.id);
+});
+
+app.get('/todos',function(res,req){
+	res.json(todos);
+});
