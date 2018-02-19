11e61146f3e3ad208d36311f794ec975479c6e7f
Vinaysastha <vvvsgvinaysastha@gmail.com>
2018-02-19 11:07:27 +0530

    Changed the server.js

--- server.js
+++ server.js
@@ -3,29 +3,30 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
 	id: 1,
-	description: 'Meet her at the bus station',
-	compleated: false
+	description: 'Meet mom for lunch',
+	completed: false
 }, {
 	id: 2,
-	description: 'return home',
-	compleated: false
+	description: 'Go to market',
+	completed: false
 }, {
 	id: 3,
-	description: 'Goto sleep',
-	compleated: false
+	description: 'Feed the cat',
+	completed: true
 }];
-app.get('/', function(req, res) {
-	res.send('Todo API Root');
-})
-app.listen(PORT, function() {
-	console.log('Express listining on PORT: ' + PORT + '!');
-})
-// app.get('/todos/:id',function (req, res) {
-// 	var todoId = req.params.id;
-// 	var matchedId
-// 	res.send('Asking for todo with id of '+ req.params.id);
-// })
-app.get('/todos', function(res, req) {
-	res.json(todos);
 
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
 });
